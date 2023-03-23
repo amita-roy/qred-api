09ce6c57cd78fe93ab69f5287ec19163289482f2
Amita Roy <sardana.amita2@gmail.com>
2023-03-23 11:41:22 +0100

    Add Swagger UI route

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,14 @@
 import {app} from "./app";
+import swaggerUi from "swagger-ui-express";
+import fs from "node:fs";
+import YAML from "yaml";
+
 const port = 3010
+
+const file  = fs.readFileSync('./openapi.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
     console.log(`Server has started on http://localhost:${port}`)
